72c1b6c6b26978664798ba4693f5e5bc9525e93b
salmonco <xkqjsslsek80@naver.com>
2024-05-21 14:09:36 +0900

    Design: ott page

---
+++ src/components/dramaItem.js
@@ -0,0 +1,8 @@
+export default function DramaItem() {
+  return (
+    <div className="flex flex-col gap-[8px] items-center">
+      <div className="w-[100px] h-[140px] bg-slate-200"></div>
+      <span>드라마 제목</span>
+    </div>
+  );
+}
--- src/pages/ott.js
+++ src/pages/ott.js
@@ -1,10 +1,34 @@
+import BottomTabNav from "@components/bottomTabNav";
+import DramaItem from "@components/dramaItem";
+import OttItem from "@components/ottItem";
+
 export default function Ott() {
+  const MAX_SELECT_CNT = 3;
+
   return (
-    <div>
-      <div className="grid grid-cols-3 justify-items-center">
-        <div className="w-[100px] h-[140px] bg-slate-200">드라마</div>
-        <div className="w-[100px] h-[140px] bg-slate-200">드라마</div>
-        <div className="w-[100px] h-[140px] bg-slate-200">드라마</div>
+    <div className="overflow-y-auto flex flex-col gap-[16px] px-[16px] pt-[20px] pb-[80px]">
+      <span>
+        현재 {1}개 선택 / {MAX_SELECT_CNT}
+      </span>
+      <div className="grid grid-cols-3 justify-items-center gap-y-[16px]">
+        <DramaItem />
+        <DramaItem />
+        <DramaItem />
+        <DramaItem />
+        <DramaItem />
+        <DramaItem />
+      </div>
+      <button className="py-[10px] px-[16px] bg-brand text-white disabled:bg-slate-300 disabled:text-black rounded-[10px]">
+        OTT 추천받기
+      </button>
+      <div className="flex flex-col gap-[10px]">
+        <OttItem />
+        <OttItem />
+        <OttItem />
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full z-10">
+        <BottomTabNav />
       </div>
     </div>
   );
