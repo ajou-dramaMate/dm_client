a7987a810b3bcc6b66ae782e87c520615e96334d
sha <shache.ovo@gmail.com>
2024-06-06 03:10:26 +0900

    feat: fetch post detail page

--- src/components/commentItem.js
+++ src/components/commentItem.js
@@ -1,10 +1,10 @@
-export default function CommentItem() {
+export default function CommentItem({ comment }) {
   return (
     <div className="flex flex-col gap-[4px] bg-slate-200 py-[10px] px-[16px]">
-      <span>댓글 내용</span>
+      <span>{comment.text}</span>
       <div className="flex gap-[4px] text-[1.4rem] font-m">
-        <span>작성자</span>
-        <span>작성일</span>
+        <span>{comment.name}</span>
+        <span>{comment.date}</span>
       </div>
     </div>
   );
--- src/pages/postDetail.js
+++ src/pages/postDetail.js
@@ -1,43 +1,105 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import BottomTabNav from "@components/bottomTabNav";
 import CommentItem from "@components/commentItem";
 
 export default function PostDetail() {
   const router = useRouter();
-  const { postType } = router.query;
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState("");
+
+  const handleCommentText = (event) => {
+    setCommentText(event.target.value);
+  };
+
+  async function fetchPost() {
+    const { communityId } = router.query;
+    if (!communityId) return;
+
+    try {
+      const response = await fetch(`/api/api/v1/community/${communityId}`);
+      const data = await response.json();
+      console.log(data);
+      setPost(data);
+      setComments(data.comments);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPost();
+  }, [router.query.communityId]);
+
+  const handleCommentSubmit = async (event) => {
+    event.preventDefault();
+
+    const newComment = {
+      communityId: +router.query.communityId,
+      text: commentText,
+    };
+
+    try {
+      const response = await fetch("/api/api/v1/comment", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newComment),
+      });
+      console.log(response);
+
+      setComments((prevComments) => [...prevComments, newComment]);
+      setCommentText("");
+      fetchPost();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="overflow-y-auto flex flex-col gap-[30px] px-[16px] pt-[50px] pb-[80px]">
       <div>
-        <span className="font-b">커뮤니티 글 제목</span>
+        <span className="font-b">{post.title}</span>
         <div className="flex gap-[7px] text-[1.4rem] font-m">
-          <span>작성자</span>
-          <span>작성일</span>
-          {postType === "chat" && <span>스포일러</span>}
+          <span>{post.name}</span>
+          <span>{post.date}</span>
+          {post.spoiler && <span>스포일러</span>}
         </div>
-        {postType === "recruit" && (
+        {post.totalRecruit && (
           <div className="flex gap-[7px] text-[1.4rem] font-m">
-            <span>OTT</span>
-            <span>현재 모집 인원</span>
-            <span>전체 모집 인원</span>
+            <span>{post.ott}</span>
+            <span>
+              {post.currentRecruit}/{post.totalRecruit}
+            </span>
           </div>
         )}
       </div>
-      <span>커뮤니티 글 내용</span>
+      <span>{post.contents}</span>
 
       <div className="flex flex-col gap-[4px]">
-        <CommentItem />
-        <CommentItem />
-        <CommentItem />
+        {comments.map((comment) => (
+          <CommentItem key={`${Math.random()}`} comment={comment} />
+        ))}
       </div>
 
-      <form className="relative flex flex-col gap-[4px] py-[10px] pl-[16px] pr-[50px] bg-slate-300">
+      <form
+        className="relative flex flex-col gap-[4px] py-[10px] pl-[16px] pr-[50px] bg-slate-300"
+        onSubmit={handleCommentSubmit}
+      >
         <input
           className="py-[3px] px-[5px]"
           type="text"
+          value={commentText}
+          onChange={handleCommentText}
           placeholder="댓글을 작성해 주세요."
         />
-        <button className="absolute top-1/2 -translate-y-1/2 right-[16px]">
+        <button
+          type="submit"
+          className="absolute top-1/2 -translate-y-1/2 right-[16px]"
+        >
           등록
         </button>
       </form>
