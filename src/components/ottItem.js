06fee15e6c3476d3faf4ed490837fc1e0e3b3f10
salmonco <xkqjsslsek80@naver.com>
2024-06-06 23:59:46 +0900

    Design: ott 가격 임의로 설정

--- src/components/ottItem.js
+++ src/components/ottItem.js
@@ -23,11 +23,21 @@ export default function OttItem({ item, i }) {
           className="w-[170px] h-[50px] bg-slate-200"
         />
         <div className="flex flex-col gap-[4px]">
-          <div className="flex gap-[4px]">
+          <div className="flex gap-[7px] items-center">
             <span className="font-b">
+              {i === 0
+                ? "7,900원 / 월"
+                : i === 1
+                ? "7,900원 / 월"
+                : "5,000원 / 월"}
+            </span>
+            <span className="text-[#9DA0A8] text-[1.3rem]">
+              {i === 0 ? "베이직" : i === 1 ? "베이직" : "광고형 스탠다드"}
+            </span>
+            {/* <span className="font-b">
               {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원 /
               월
-            </span>
+            </span> */}
           </div>
           <span className="font-m text-[1.3rem] text-[#3B3F4A]">
             {item.dramas.join(", ")}
