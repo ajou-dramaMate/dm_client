85791ff1473a92324f0e09eabec6993089c82e83
salmonco <xkqjsslsek80@naver.com>
2024-06-06 12:48:07 +0900

    Design: community

--- src/components/postItem.js
+++ src/components/postItem.js
@@ -3,21 +3,26 @@ import Link from "next/link";
 export default function PostItem({ post }) {
   return (
     <Link href={`/postDetail?communityId=${post.communityId}`}>
-      <div className="flex flex-col gap-[4px] bg-slate-200 rounded-[5px] py-[20px] px-[20px]">
-        <span className="font-b break-words">{post.title}</span>
-        <div className="flex gap-[7px] text-[1.4rem] font-m">
+      <div className="flex flex-col gap-[8px] text-[1.4rem] bg-[#2d1f720d] py-[10px] px-[16px]">
+        <div className="flex gap-[8px] items-center justify-between">
           <span>{post.name}</span>
-          <span>{post.date}</span>
-          {post.spoiler && <span>스포일러</span>}
-        </div>
-        {post.totalRecruit && (
-          <div className="flex gap-[7px] text-[1.4rem] font-m">
-            <span>{post.ott}</span>
-            <span>
-              {post.currentRecruit}/{post.totalRecruit}
-            </span>
+
+          <div className="flex gap-[8px] items-center text-[#3B3F4A] text-[1.3rem]">
+            <div className="flex gap-[4px] items-center">
+              {post.spoiler && <span>스포일러 포함</span>}
+              {post.totalRecruit && (
+                <div className="flex gap-[4px] items-center font-m">
+                  <span>{post.ott}</span>
+                  <span>
+                    {post.currentRecruit}/{post.totalRecruit}
+                  </span>
+                </div>
+              )}
+            </div>
+            <span className="text-[#9DA0A8]">{post.date}</span>
           </div>
-        )}
+        </div>
+        <span className="font-b break-words">{post.title}</span>
       </div>
     </Link>
   );
